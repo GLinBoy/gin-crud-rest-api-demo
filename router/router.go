f4c03583065152c250ed43e7055ba230e663792a
GLinBoy <glinboy@live.com>
2024-07-29 18:55:32 +0200

    🐛 FIX: Updated name of update movie method

--- router/router.go
+++ router/router.go
@@ -12,7 +12,7 @@ func InitRouter() *gin.Engine {
 	r.GET("/movies", getMovies)
 	r.GET("/movies/:id", getMovie)
 	r.POST("/movies", postMovie)
-	r.PUT("/movies/:id", updateMovie)
+	r.PUT("/movies/:id", putMovie)
 	r.DELETE("/movies/:id", deleteMovie)
 
 	return r
